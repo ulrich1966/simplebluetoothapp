ba6e3b0caf9321d2736729a523830102da1b0a9a
Uli <post2uli@gmx.de>
2019-03-11 09:27:23 +0100

    init

---
+++ app/src/main/java/de/auli/simplebluetoothapp/MainActivity.java
@@ -0,0 +1,29 @@
+package de.auli.simplebluetoothapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickOn(View view) {
+    }
+
+    public void onClickOff(View view) {
+    }
+
+    public void onClickVisible(View view) {
+    }
+
+    public void onClickDivices(View view) {
+    }
+
+    public void onClickSearch(View view) {
+    }
+}
