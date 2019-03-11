979810699cfeefc1445a1c0c27ae473a9a72c49a
Uli <post2uli@gmx.de>
2019-03-11 10:08:20 +0100

    main activity

--- app/src/main/java/de/auli/simplebluetoothapp/MainActivity.java
+++ app/src/main/java/de/auli/simplebluetoothapp/MainActivity.java
@@ -1,10 +1,19 @@
 package de.auli.simplebluetoothapp;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView liviDevices;
+    private BluetoothAdapter bluetoothAdapter;
+    private ArrayAdapter<String> arrayAdapter;
+    private List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
